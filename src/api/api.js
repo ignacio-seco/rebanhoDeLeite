cba7eb01ab96a12f1661ed3518ba27c0a13cb440
Ignácio Seco <itenorioseco@gmail.com>
2022-12-14 12:22:33 -0300

    deploy com api

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiURLs = {
   development: 'http://localhost:8080',
-  production: 'futuro link da api',
+  production: 'https://controlegadoapp.cyclic.app',
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
